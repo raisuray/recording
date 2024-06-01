00d899d28da061acc4bb49ec33790ac313874800
raymond.widjaja10 <raymond.widjaja10@gmail.com>
2024-06-01 04:47:32 +0000

    change firebase config

--- firebase.js
+++ firebase.js
@@ -3,19 +3,20 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD7dJ98sfwTYPng8Dj4Puig80wFZwMspjo",
-  authDomain: "recording-b31f3.firebaseapp.com",
-  projectId: "recording-b31f3",
-  storageBucket: "recording-b31f3.appspot.com",
-  messagingSenderId: "890540693047",
-  appId: "1:890540693047:web:e1969c106c1f61122911b2",
-  measurementId: "G-H9QLZTL0C5"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
